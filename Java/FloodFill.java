964eafe225f1d7cdf9943e89005597b0a92ff601
burnjoe <sabanajholo@gmail.com>
2023-02-04 21:03:35 +0800

    Updated FloodFill.java

--- Java/FloodFill.java
+++ Java/FloodFill.java
@@ -1,12 +1,13 @@
 package leetcode.Java;
 
 class FloodFill {
-    public static int[][] floodFill(int[][] image, int sr, int sc, int color) {
+
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         floodFill(image, sr, sc, color, image[sr][sc]);
         return image;
     }
      
-    public static void floodFill(int[][] image, int sr, int sc, int color, int old) {
+    public void floodFill(int[][] image, int sr, int sc, int color, int old) {
         if(sr >= 0 && sc >= 0 && sr < image.length && sc < image[sr].length) {
             if(image[sr][sc] != old || image[sr][sc] == color) {
                 return;
@@ -14,10 +15,11 @@ class FloodFill {
                 image[sr][sc] = color; 
             }
              
-            floodFill(image, sr-1, sc, color, old);	// up
-            floodFill(image, sr, sc+1, color, old); // right
-            floodFill(image, sr+1, sc, color, old);	// down
-            floodFill(image, sr, sc-1, color, old);	// left
+            floodFill(image, sr-1, sc, color, old);	    // up
+            floodFill(image, sr, sc+1, color, old);     // right
+            floodFill(image, sr+1, sc, color, old);	    // down
+            floodFill(image, sr, sc-1, color, old);	    // left
          }
      }
+
  }
